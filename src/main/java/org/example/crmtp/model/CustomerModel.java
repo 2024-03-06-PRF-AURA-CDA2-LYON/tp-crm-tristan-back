29551e3b3868d50fc5574dd3bd4bba5ee57a7377
Titossaniti <tristan.erard@gmail.com>
2024-10-02 17:06:35 +0200

    All request work - missing enum to fix STATUS

--- src/main/java/org/example/crmtp/model/CustomerModel.java
+++ src/main/java/org/example/crmtp/model/CustomerModel.java
@@ -1,6 +1,5 @@
 package org.example.crmtp.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
--- src/main/java/org/example/crmtp/model/OrderModel.java
+++ src/main/java/org/example/crmtp/model/OrderModel.java
@@ -1,7 +1,6 @@
 package org.example.crmtp.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
