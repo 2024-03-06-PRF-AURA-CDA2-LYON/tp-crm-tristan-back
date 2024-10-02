254daf393ce23599dfbacc034cd4d913c520144a
Titossaniti <tristan.erard@gmail.com>
2024-10-02 14:39:06 +0200

    first commit

---
+++ src/main/java/org/example/crmtp/CrmTpApplication.java
@@ -0,0 +1,13 @@
+package org.example.crmtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmTpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmTpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/crmtp/CrmTpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crmtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmTpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
